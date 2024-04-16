1ebd3ce9d2945fa9e3f216b2ff365889b3b5a922
Benjamin Heng <ben@hbenjamin.com>
2024-04-17 01:12:53 +0800

    Update go env vars

--- desktop/fish/.config/fish/config.fish
+++ desktop/fish/.config/fish/config.fish
@@ -3,15 +3,15 @@ if status is-interactive
     ssh-add ~/.ssh/id_ed25519_github > /dev/null 2>&1
 end
 
-set GOPATH "$HOME/dev/go"
-set GOBIN "$GOPATH/bin"
+set -g -x GOPATH "$HOME/dev/go"
+set -g -x GOBIN "$GOPATH/bin"
 set PATH $PATH ~/bin
 set PATH $PATH ~/dev/private-dotfiles/bin
 set PATH $GOBIN $PATH
 set VISUAL "nvim"
 set EDITOR "$VISUAL"
 set GIT_EDITOR "$VISUAL"
-set GOPRIVATE github.com/benjaminheng
+set -g -x GOPRIVATE github.com/benjaminheng
 set -x LEDGER_FILE "/home/ben/syncthing/hledger/finances.journal"
 
 # Set prompt
